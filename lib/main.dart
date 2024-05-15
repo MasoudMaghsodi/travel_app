7a6126d44a877661c028891c57de8d3e961d5f67
MasoudMaghsodi <magsodi333@gmail.com>
2024-05-16 02:47:26 +0330

    finish project

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
-import 'dart:ffi';
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
+import 'package:expandable_text/expandable_text.dart';
 import 'package:travel_app/model/TravelModel.dart';
 
 void main() {
@@ -37,122 +35,123 @@ class _MainPageState extends State<MainPage> {
     var size = MediaQuery.of(context).size;
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          children: [
-            Container(
-              color: Colors.white,
-              width: double.infinity,
-              height: size.height / 1.8,
-              child: Stack(
-                children: [
-                  Container(
-                    width: double.infinity,
-                    height: size.height / 2.1,
-                    decoration: BoxDecoration(
-                      borderRadius: const BorderRadius.only(
-                          bottomLeft: Radius.circular(60),
-                          bottomRight: Radius.circular(60)),
-                      image: DecorationImage(
-                        image: AssetImage(travelList[_selectedIndex].image),
-                        fit: BoxFit.cover,
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Container(
+                color: Colors.white,
+                width: double.infinity,
+                height: size.height / 1.8,
+                child: Stack(
+                  children: [
+                    Container(
+                      width: double.infinity,
+                      height: size.height / 2.1,
+                      decoration: BoxDecoration(
+                        borderRadius: const BorderRadius.only(
+                            bottomLeft: Radius.circular(60),
+                            bottomRight: Radius.circular(60)),
+                        image: DecorationImage(
+                          image: AssetImage(travelList[_selectedIndex].image),
+                          fit: BoxFit.cover,
+                        ),
                       ),
                     ),
-                  ),
-                  //head icons
-                  Positioned(
-                    right: 0,
-                    top: 0,
-                    left: 0,
-                    child: Padding(
-                      padding: const EdgeInsets.fromLTRB(16, 20, 16, 0),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    //head icons
+                    Positioned(
+                      right: 0,
+                      top: 0,
+                      left: 0,
+                      child: Padding(
+                        padding: const EdgeInsets.fromLTRB(16, 20, 16, 0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Container(
+                              width: 50,
+                              height: 50,
+                              decoration: const BoxDecoration(
+                                shape: BoxShape.circle,
+                                color: Color.fromARGB(101, 255, 255, 255),
+                              ),
+                              child: const Icon(CupertinoIcons.lessthan),
+                            ),
+                            Container(
+                              width: 50,
+                              height: 50,
+                              decoration: const BoxDecoration(
+                                shape: BoxShape.circle,
+                                color: Color.fromARGB(101, 255, 255, 255),
+                              ),
+                              child: const Icon(CupertinoIcons.heart),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    // image list
+                    Positioned(
+                      right: 0,
+                      top: 100,
+                      child: SizedBox(
+                        width: 90,
+                        height: double.maxFinite,
+                        child: ListView.builder(
+                          itemCount: travelList.length,
+                          itemBuilder: (context, index) {
+                            return imageItem(index);
+                          },
+                        ),
+                      ),
+                    ),
+                    // name and place
+                    Positioned(
+                      bottom: 80,
+                      left: size.width / 9,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
-                          Container(
-                            width: 50,
-                            height: 50,
-                            decoration: const BoxDecoration(
-                              shape: BoxShape.circle,
-                              color: Color.fromARGB(101, 255, 255, 255),
+                          Text(
+                            travelList[_selectedIndex].name,
+                            style: const TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20,
                             ),
-                            child: const Icon(CupertinoIcons.lessthan),
                           ),
-                          Container(
-                            width: 50,
-                            height: 50,
-                            decoration: const BoxDecoration(
-                              shape: BoxShape.circle,
-                              color: Color.fromARGB(101, 255, 255, 255),
-                            ),
-                            child: const Icon(CupertinoIcons.heart),
+                          const SizedBox(
+                            height: 8,
                           ),
+                          Row(
+                            children: [
+                              const Icon(
+                                CupertinoIcons.placemark_fill,
+                                color: Colors.white,
+                              ),
+                              Text(
+                                travelList[_selectedIndex].location,
+                                style: const TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 14,
+                                ),
+                              ),
+                            ],
+                          )
                         ],
                       ),
                     ),
-                  ),
-                  // image list
-                  Positioned(
-                    right: 0,
-                    top: 100,
-                    child: SizedBox(
-                      width: 90,
-                      height: double.maxFinite,
-                      child: ListView.builder(
-                        itemCount: travelList.length,
-                        itemBuilder: (context, index) {
-                          return imageItem(index);
-                        },
-                      ),
-                    ),
-                  ),
-                  // name and place
-                  Positioned(
-                    bottom: 80,
-                    left: size.width / 9,
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          travelList[_selectedIndex].name,
-                          style: const TextStyle(
-                            color: Colors.white,
-                            fontWeight: FontWeight.bold,
-                            fontSize: 20,
-                          ),
-                        ),
-                        const SizedBox(
-                          height: 8,
-                        ),
-                        Row(
-                          children: [
-                            const Icon(
-                              CupertinoIcons.placemark_fill,
-                              color: Colors.white,
-                            ),
-                            Text(
-                              travelList[_selectedIndex].location,
-                              style: const TextStyle(
-                                color: Colors.white,
-                                fontWeight: FontWeight.bold,
-                                fontSize: 14,
-                              ),
-                            ),
-                          ],
-                        )
-                      ],
-                    ),
-                  ),
-                ],
+                  ],
+                ),
               ),
-            ),
-            Expanded(
-              child: Container(
+              Container(
                 width: double.infinity,
                 child: Column(
                   children: [
                     Row(
                       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                       children: [
+                        // card
                         Card(
                           shape: RoundedRectangleBorder(
                             borderRadius: BorderRadius.circular(20),
@@ -161,6 +160,7 @@ class _MainPageState extends State<MainPage> {
                               width: 1,
                             ),
                           ),
+                          // distance card
                           child: Container(
                             width: 85,
                             height: 85,
@@ -175,6 +175,7 @@ class _MainPageState extends State<MainPage> {
                             ),
                           ),
                         ),
+                        // temo card
                         Card(
                           shape: RoundedRectangleBorder(
                             borderRadius: BorderRadius.circular(20),
@@ -197,6 +198,7 @@ class _MainPageState extends State<MainPage> {
                             ),
                           ),
                         ),
+                        // rating card
                         Card(
                           shape: RoundedRectangleBorder(
                             borderRadius: BorderRadius.circular(20),
@@ -220,12 +222,62 @@ class _MainPageState extends State<MainPage> {
                           ),
                         ),
                       ],
-                    )
+                    ),
+                    // discreption
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(24, 16, 24, 16),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const Text('Discreption'),
+                          ExpandableText(
+                            travelList[_selectedIndex].discription,
+                            expandText: 'read more',
+                            collapseText: 'show less',
+                            textDirection: TextDirection.ltr,
+                          ),
+                          const SizedBox(
+                            height: 16,
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  const Text('Total Price'),
+                                  Text(
+                                    '${travelList[_selectedIndex].price}\$',
+                                    style: const TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 30,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              Container(
+                                width: 100,
+                                height: 100,
+                                decoration: const BoxDecoration(
+                                  shape: BoxShape.circle,
+                                  color: Colors.black,
+                                ),
+                                child: const Icon(
+                                  CupertinoIcons.arrow_right,
+                                  color: Colors.white,
+                                  size: 30,
+                                ),
+                              ),
+                            ],
+                          )
+                        ],
+                      ),
+                    ),
                   ],
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
